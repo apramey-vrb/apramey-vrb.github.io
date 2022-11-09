9d3153420225ab94690ae89c4f55fbb85ec1239b
apramey bhat <aprameya.rb@gmail.com>
2022-11-09 22:25:00 +0200

    with new stuffs

--- _nuxt/static/1667943564/blog/2019-09-22-veggies/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667943564",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2019-09-22-veggies",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"12b4867d"}},_img:{"/_ipx/_/author.png":"\u002F_nuxt\u002Fimage\u002Ff10964.png"}}}(null));
